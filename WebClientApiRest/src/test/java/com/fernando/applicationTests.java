aa06e6b3783101f8bb6f8e69ca43b1048f20d137
Fernando <fernandobrambillab1@hotmail.com>
2023-08-01 14:30:04 -0300

    Created Project

---
+++ WebClientApiRest/src/test/java/com/fernando/applicationTests.java
@@ -0,0 +1,13 @@
+package com.fernando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class applicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
